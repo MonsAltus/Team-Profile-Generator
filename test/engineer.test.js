b6d87eb2d6a82b91dfb1c5b5e7c66c823c64b956
James <James.Hacklander@gmail.com>
2021-05-14 16:26:27 -0700

    Fixed errors in test files.

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -2,13 +2,13 @@ const Employee = require('../lib/employee')
 const Engineer = require('../lib/engineer')
 
 test('Can set Github through constructor argument', () => {
-    const testValue = testUsername
+    const testValue = 'testUsername'
     const e = new Engineer('Foo', 1, 'test@email.com', testValue)
     expect(e.github).toBe(testValue)
 })
 
 test('Can get Github from getGithub()', () => {
-    const testValue = testUsername
+    const testValue = 'testUsername'
     const e = new Engineer('Foo', 1, 'test@email.com', testValue)
     expect(e.getGithub()).toBe(testValue)
 })
