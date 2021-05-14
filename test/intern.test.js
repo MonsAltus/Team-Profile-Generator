bd27ddbacb2d5fd5ce659c9eb6c1640b564574aa
James <James.Hacklander@gmail.com>
2021-05-14 16:25:28 -0700

    Fixed errors in test files.

--- test/intern.test.js
+++ test/intern.test.js
@@ -2,13 +2,13 @@ const Employee = require('../lib/employee')
 const Intern = require('../lib/intern')
 
 test('Can set school through constructor argument', () => {
-    const testValue = 10
+    const testValue = 'UCSD'
     const e = new Intern('Foo', 1, 'test@email.com', testValue)
     expect(e.school).toBe(testValue)
 })
 
 test('Can get school from getSchool()', () => {
-    const testValue = 10
+    const testValue = 'UCSD'
     const e = new Intern('Foo', 1, 'test@email.com', testValue)
     expect(e.getSchool()).toBe(testValue)
 })
